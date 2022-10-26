72cc27a256b33eeb9e84211db434b75594774cfd
Jose Antonio Ponqueli Conto <zehconto@gmail.com>
2022-10-25 22:26:41 -0300

    feat: header and input component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,30 @@
-import { globalStyles } from "./styles/global";
+import { PlusCircle } from "phosphor-react";
+import { Header } from "./components/Header";
+import styles from "./App.module.css";
+import "./global.css";
 
 function App() {
   return (
     <>
-      <h1>Hello World</h1>
-      {globalStyles()}
+      <Header />
+      <main>
+        <div className={styles.inputGroup}>
+          <input
+            // onKeyDown={(e) => e.key === "Enter" && handleCreateNewTask()}
+            type="text"
+            placeholder="Adicione uma nova tarefa"
+          // onChange={(e) => setNewTaskTitle(e.target.value)}
+          // value={newTaskTitle}
+          />
+          <button
+            type="submit"
+          // onClick={handleCreateNewTask}
+          >
+            Criar
+            <PlusCircle size={20} color="var(--white)" />
+          </button>
+        </div>
+      </main>
     </>
   );
 }

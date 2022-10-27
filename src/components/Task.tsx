d5da26833e3acd003cd6d7e12b914bdc46062ef9
Jose Antonio Ponqueli Conto <zehconto@gmail.com>
2022-10-26 21:37:54 -0300

    style task

--- src/components/Task.tsx
+++ src/components/Task.tsx
@@ -4,10 +4,10 @@ import styles from "./Task.module.css";
 export function Task() {
   return (
     <div className={styles.container}>
-      <input type="checkbox" />
+      <input type="checkbox" className={styles.checkBox} />
       <p>Estudar React</p>
       <button type="button">
-        <Trash size={32} />
+        <Trash size={24} />
       </button>
     </div>
   );
